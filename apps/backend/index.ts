5df917e2afe815e7a4c894413db29957f16392c4
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-10-06 19:04:48 -0400

    use static plugin

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,4 +1,5 @@
 import { html } from "@elysiajs/html";
+import staticPlugin from "@elysiajs/static";
 import Elysia from "elysia";
 import path from "path";
 const app = new Elysia();
@@ -13,41 +14,14 @@ app
     context.set.status = 500;
     return "Internal Server Error";
   })
-  .get("/website/*", async (context) => {
-    const assetsRegex = /\.(js|css|png|jpg|jpeg)$/;
-    if (assetsRegex.test(context.request.url)) {
-      const fileName = new URL(context.request.url).pathname.split("/").pop();
-      if (!fileName) throw new Error("Not Found");
-
-      const asset = Bun.file(path.resolve(import.meta.dir, "..", "..", "dist", "assets", fileName));
-      if (!(await asset.exists())) throw new Error("Not Found");
-
-      const data = await asset.text();
-      return new Response(data, { headers: { "Content-Type": asset.type } });
-    }
-
-    const indexHtml = Bun.file(path.resolve(import.meta.dir, "..", "..", "dist", "index.html"));
-    if (!(await indexHtml.exists())) throw new Error("Not Found");
-
-    const html = await indexHtml.text();
-    return context.html(html);
+  .get("/website/*", () => {
+    return Bun.file(path.resolve(import.meta.dir, "../../dist/index.html"));
   })
+  .use(
+    staticPlugin({
+      prefix: "/website",
+      assets: path.resolve(import.meta.dir, "../../dist"),
+      alwaysStatic: true,
+    }),
+  )
   .listen(3000);
-
-// import express from "express";
-// const app = express();
-// const DIST_DIR = path.resolve(import.meta.dir, "..", "..", "dist");
-
-// app.use("/website", express.static(DIST_DIR));
-// app.get("/website/*", (request, response) => {
-//   const indexPath = path.join(DIST_DIR, "index.html");
-//   response.sendFile(indexPath);
-// });
-
-// app.use((_request, response) => {
-//   response.status(404).send("Not Found");
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
